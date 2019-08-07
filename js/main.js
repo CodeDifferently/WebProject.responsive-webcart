dd7809ab647f7175a4b5e0ab13bb7b30b9364700
CodeDifferently <leonhunter@Leons-MacBook-Pro-2.local>
2019-08-07 18:57:55 -0400

    first update

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/** import `utils.js` */
+document.write('<script type="text/javascript" src="./js/utils.js"></script>');
+
+/* Set rates + misc */
+var taxRate = 0.05;
+var shippingRate = 15.00;
+var fadeTime = 300;
+
+
+/* Assign actions */
+$('.product-quantity input').change( function() {
+  updateQuantity(this);
+});
+
+$('.product-removal button').click( function() {
+  removeItem(this);
+});
+
